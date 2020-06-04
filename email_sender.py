ab795585b212ce99dd84156316e767f39b6ba82f
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-04 10:43:27 +0900

    added html template

--- email_sender.py
+++ email_sender.py
@@ -1,16 +1,19 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'your_name'
-email['to'] = 'target_here_email'
+email['to'] = 'target_email_address'
 email['subject'] = 'Hi there This is a Test Mail'
 
-email.set_content('hihi, hellllo')
+email.set_content(html.substitute({'name': 'hoyi'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('your_email', 'your_password')
+    smtp.login('your_email_here', 'your_password_here')
     smtp.send_message(email)
     print('all good boss!')
